be1608aa994e79e7d8570a0bab3e5736cddc1329
lack <598223084@qq.com>
2021-03-23 09:52:03 +0800

    modify

--- build.sh
+++ build.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 vine="github.com/lack-io/vine"
-mods=`find . -name "go.mod" | grep -v "vendor"`
+mods=$(find . -name "go.mod" | grep -v "vendor")
 
 for mod in $mods;do
-  flag=`cat $mod | grep $vine`
+  flag=$(cat "$mod" | grep $vine)
   if [[ -n $flag ]];then
-    v=`echo $flag | awk -F' ' '{print $NF}'`
-    vv=`git tag --list | grep $v`
+    v=$(echo "${flag}" | awk -F' ' '{print $NF}')
+    dir=$(dirname "$mod")
+    version=${dir:2}/$v
+    vv=$(git tag --list | grep "$version")
     if [[ -z $vv ]];then
-      dir=`dirname $mod`
-      git add $dir
-      version=${dir:2}/$v
+      git add .
       git commit -m "$version"
-      echo $version
-#      git tag $version
+      git tag "$version"
+      echo "add tag $version"
     fi
   fi
 done
