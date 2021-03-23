ef982937cf0ee9f336d211e6485890f6e6c0913a
lack <598223084@qq.com>
2021-03-23 09:10:29 +0800

    dao sqlite

--- dao/sqlite/sqlite.go
+++ dao/sqlite/sqlite.go
@@ -10,7 +10,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package sqlite3
+package sqlite
 
 import (
 	"github.com/lack-io/vine/service/dao"
